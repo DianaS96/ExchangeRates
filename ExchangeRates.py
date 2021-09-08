ad4f19a219d11d35135d0aee2a9ae9632bee4973
Diana Sibagatullina <diana.sibagatullina96@gmail.com>
2021-09-08 20:59:01 +0300

    Downloading FOREX from cbr.ru for the PERIOD

--- ExchangeRates.py
+++ ExchangeRates.py
@@ -3,25 +3,19 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import time
-import sys
-import os
 
 # Function check_date() check whether input date is correct. If date is invalid, you should enter the date again
 def check_date():
     flag = 0
     today = time.localtime() # Current date
-    #print(today)
-    #print(tomorrow)
 
     while flag == 0:
         day = input("Enter day: ")
         month = input("Enter month: ")
         year = input("Enter year: ")
         date_inp = day + '/' + month + '/' + year # Concatenate day, month and year
-    #   print(date_inp)
         try:
             valid_date = time.strptime(date_inp, '%d/%m/%Y')
-    #        print(valid_date)
             flag = 1
             if valid_date > today:
                 if (valid_date.tm_year == today.tm_year and valid_date.tm_mon == today.tm_mon and valid_date.tm_mday == (today.tm_mday + 1)):
@@ -48,14 +42,11 @@ def get_data(day, month, year, date_inp):
 #    print(header)
     for item in header:
         try:
-#            print(item.text)
             list_header.append(item.text)
         except:
             continue
-#    print(list_header)
 
     data = tree.find_all('table')[0].find_all('tr')[1:]
-#    print(data)
     for elem in data:
         sub_table = list()
         for sub_elem in elem:
@@ -64,17 +55,7 @@ def get_data(day, month, year, date_inp):
             except:
                 continue
         list_data.append(sub_table)
-#    print(list_data)
 
-    # parse the table
     df = pd.DataFrame(data=list_data, columns=list_header)
- #   print(df)
- #   write_data_in_file(df, date_inp)
+    # Write data in a csv file
     df.to_csv((str("Курсы на " + str(date_inp).replace('/', '.')) + ".csv"), encoding='utf-8-sig')
-# Function write_data_in_file writes data in a file
-#def write_data_in_file(df, date_inp):
-  #  df.to_csv((str("Курсы на " + str(date_inp).replace('/', '.')) + ".csv", "w"))
-    #fd.write(str(df[0]))
-    #fd.close()
-    #print(df[0])
-""""""
--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import ExchangeRates
+import FOREX_period
 
 def main():
     flag = 0
     while(flag == 0):
-        N = input("Please, choose an option: \n1 - Import official exchange rates on selected date from CBR.RU\n0 - exit\n")
+        N = input("Please, choose an option: \n1 - Import official exchange rates on selected date from CBR.RU\n\
+        2 - Dynamics of the official exchange rates\n\
+        0 - exit\n")
         if N == '1':
             ExchangeRates.check_date()
+        elif N == '2':
+            FOREX_period.check_curr()
         elif N == '0':
             flag = 1
             print("Bye!")
